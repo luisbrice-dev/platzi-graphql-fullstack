779b9593d29d02c0417e530b813d78b719b09381
Luis Briceño <siulbriceno20@gmail.com>
2025-04-07 18:00:09 -0400

    Add Prisma integration with SQLite and update Avocado model schema

--- api/src/modules/avocado/avocado.resolver.ts
+++ api/src/modules/avocado/avocado.resolver.ts
@@ -1,24 +1,25 @@
 import { createHash } from 'crypto'
 import { baseModelResolver } from '../base/base.resolver'
-import { Avocado } from './avocado.model'
+//import { Avocado } from './avocado.model'
+import type { Avocado } from '@prisma/client'
 
 const avos: Avocado[] = [
   {
     createdAt: new Date(),
-    updatedAt: undefined,
-    deletedAt: undefined,
+    updatedAt: null,
+    deletedAt: null,
     name: 'Pinkerton Avocado',
-    id: 'fpr72m9k',
+    id: 1,
     sku: 'B4HZ42TM',
     price: 1.27,
     image: '/images/pinkerton.jpg',
-    attributes: {
+    /*attributes: {
       description:
         "First grown on the Pinkerton Ranch in Saticoy, California, in the early 1970s, 'Pinkerton' is a seedling of 'Hass' x 'Rincon'. The large fruit has a small seed, and its green skin deepens in color as it ripens. The thick flesh has a smooth, creamy texture, pale green color, good flavor, and high oil content. It shows some cold tolerance, to −1 °C (30 °F) and bears consistently heavy crops. A hybrid Guatemalan type, it has excellent peeling characteristics",
       shape: 'Long pear',
       hardiness: '−1 °C',
       taste: 'Marvelous, is an avocado',
-    },
+    },*/
   },
 ]
 
@@ -37,12 +38,12 @@ export const resolver: Record<keyof Avocado, (parent: Avocado) => unknown> = {
   name: (parent) => parent.name,
   price: (parent) => parent.price,
   image: (parent) => parent.image,
-  attributes: (parent) => ({
+  /*attributes: (parent) => ({
     description: parent.attributes.description,
     shape: parent.attributes.shape,
     hardiness: parent.attributes.hardiness,
     taste: parent.attributes.taste,
-  }),
+  }),*/
 }
 
 export function createAvo(
@@ -53,23 +54,23 @@ export function createAvo(
 ): Avocado {
   const currentLength = avos.length
   const newAvo: Avocado = {
-    id: String(currentLength + 1),
+    id: currentLength + 1,
     sku: createHash('sha256')
       .update(data.name, 'utf8')
       .digest('base64')
       .slice(-6),
     createdAt: new Date(),
     updatedAt: new Date(),
-    deletedAt: undefined,
+    deletedAt: null,
     name: data.name,
     price: data.price,
     image: data.image,
-    attributes: {
+    /*attributes: {
       description: data.description,
       shape: data.shape,
       hardiness: data.hardiness,
       taste: data.taste,
-    },
+    },*/
   }
 
   avos.push(newAvo)
